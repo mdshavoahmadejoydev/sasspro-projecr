48d7d8040e8ea8d00db33dfec221d9b3e582bbd1
mdshavoahmadejoydev <mdshavoahmadejoydev@gmail.com>
2026-09-07 03:22:37 +0600

    HW-done

--- src/Components/Indistrycard.jsx
+++ src/Components/Indistrycard.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 
 import { MdKeyboardArrowRight } from "react-icons/md";
+import { FaCalendarAlt } from "react-icons/fa";
+
 
 import Img from '../Components/Img'
+import Flex from '../Components/Flex'
 
-const Indistrycard = ({src, title, des,}) => {
+const Indistrycard = ({src, title, des, showimg, proimg, proname}) => {
   return (
     <div>
-      <div className="w-366 flex justify-center flex-col shadow-2xl">
-        <Img
-          src={src}
-          alt={`img`}
-          className={`w-346 h-220 m-auto pt-2.5`}
-        />
+      <div className="w-366 flex justify-center flex-col shadow-2xl rounded-lg">
+        <Img src={src} alt={`img`} className={`w-346 h-220 m-auto pt-2.5`} />
         <div className="px-31 pb-31 pt-31">
-          <p className="font-raleway font-bold text-22 text-seagreen mb-5">
+          <p className="font-raleway font-bold text-22 text-seagreen mb-5 truncate">
             {title}
           </p>
-          <p className="font-roboto font-normal text-base text-olive mb-6">
+
+          {showimg && (
+            <Flex className={`items-center justify-between my-25`}>
+              <Flex className={`items-center`}>
+                <div className="w-30 h-30 mr-11">
+                  <img src={proimg} alt="img" className="w-full h-full"></img>
+                </div>
+                <p className="w-148 font-sm text-seagreen font-roboto font-bold truncate">
+                  {proname}
+                </p>
+              </Flex>
+
+              <Flex className={`items-center gap-2 ml-25`}>
+                <FaCalendarAlt className="text-seagreen"/>
+
+                <input
+                  type="date"
+                  className="!bg-transparent outline-none border-none font-roboto font-semibold text-seagreen [&::-webkit-calendar-picker-indicator]:hidden"
+                />
+              </Flex>
+            </Flex>
+          )}
+
+          <p className="font-roboto font-normal text-base text-olive mb-6 line-clamp-2">
             {des}
           </p>
           <p className="font-roboto font-semibold text-base text-primary">
--- src/Sections/Article.jsx
+++ src/Sections/Article.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+
+import Articlepimg1 from '../assets/articlepimg1.png'
+import Articlepimg2 from '../assets/articlepimg2.png'
+import Articlepimg3 from '../assets/articlepimg3.png'
+
+import Articleimg1 from '../assets/articleimg1.png'
+import Articleimg2 from '../assets/articleimg2.png'
+import Articleimg3 from '../assets/articleimg3.png'
+
 import Title from '../Components/Title'
+import Indistrycard from '../Components/Indistrycard'
+import Container from '../Components/Container'
+import Flex from '../Components/Flex'
 
 const Article = () => {
   return (
     <section className='pt-75 pb-100 bg-lightgray'>
       <Title content={`Recent News & Articles`} className={`text-center mb-56`}/>
+      <Container>
+        <Flex className={`justify-center gap-9`}>
+          <Indistrycard src={Articleimg1} title={`Where Should I Buy Next?`} des={`From romantic getaways to adventurous hikes, find your next...`} showimg={true} proimg={Articlepimg1} proname={`Carter Rosser`} />
+          
+          <Indistrycard src={Articleimg2} title={`Malesuada Fermentum Tortor`} des={`rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr gsfg sdfg asdrfa sadfg afsdgfa sgdf gsdg ssdfg`} showimg={true} proimg={Articlepimg2} proname={`Carter Rosser`} />
+          
+          <Indistrycard src={Articleimg3} title={`Magna Malesuada`} des={`From romantic getaways to adventurous hikes, find your next...`} showimg={true} proimg={Articlepimg3} proname={`Carter Rosser`} />
+
+        </Flex>
+
+      </Container>
+      
 
       
       
