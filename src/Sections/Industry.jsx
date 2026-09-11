320a6bc039d179a78f2a3287594b7cf36e5ec9c3
mdshavoahmadejoydev <mdshavoahmadejoydev@gmail.com>
2026-09-12 02:13:25 +0600

    industry part

--- src/Sections/Industry.jsx
+++ src/Sections/Industry.jsx
@@ -17,8 +17,8 @@ import Indistrycard from '../Components/Indistrycard';
 const Industry = () => {
   return (
     <section>
-      <Title content={`Industries in focus`} className={`text-center mb-56`}/>
-      <Container className={`pb-100`}>
+      <Title content={`Industries in focus`} className={`text-center mb-6 lg:mb-56`}/>
+      <Container className={`pb-14 lg:pb-100`}>
         <Flex className={`w-[100vw] lg:w-auto flex-col items-center lg:flex-row lg:flex-wrap lg:justify-center gap-9`}>
           <Indistrycard src={Industryimg1} title={`FinTech`} des={`Psum dolor sit amet, consectetur adipiscing elit. Nisi, maecenas orci sit pellentesque eget.`}/>
           
